c496ebb39e17fe1e9ed565c8a45f3b12fae62ef9
Anosua Roy <64733993+anosua@users.noreply.github.com>
2023-03-22 14:37:37 +1100

    Create example.c

---
+++ drafts/example.c
@@ -0,0 +1,9 @@
+// this is an example of a draft file to get my motor working
+// before integrating with the rest of the system
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello\n");
+    return 0;
+}
